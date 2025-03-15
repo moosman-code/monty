c30271ac4aa30f1d93f5471f401010c546f01532
Hristiyan Denkov <hristiyan.denkov@sap.com>
2025-03-15 21:31:15 +0200

    Fix ApplicationConfig

--- src/main/java/bg/uni/sofia/hiker/monti/config/ApplicationConfig.java
+++ src/main/java/bg/uni/sofia/hiker/monti/config/ApplicationConfig.java
@@ -3,6 +3,7 @@ package bg.uni.sofia.hiker.monti.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import bg.uni.sofia.hiker.monti.controller.MontiController;
 import bg.uni.sofia.hiker.monti.kafka.topic.TopicInitializer;
 import bg.uni.sofia.hiker.monti.peaks.JsonFileToPeak;
 
@@ -17,5 +18,10 @@ public class ApplicationConfig {
     @Bean(initMethod = "init")
     public JsonFileToPeak jsonFileToPeak() {
         return new JsonFileToPeak();
+    }
+
+    @Bean
+    public MontiController montiController() {
+        return new MontiController();
     }
 }
