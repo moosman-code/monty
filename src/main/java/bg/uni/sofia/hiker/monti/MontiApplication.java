9c8676fe9743cf7a6e8ecb327f034d80e5b7672a
Hristiyan Denkov <hristiyan.denkov@sap.com>
2025-03-15 18:04:54 +0200

    Initial project setup

---
+++ src/main/java/bg/uni/sofia/hiker/monti/MontiApplication.java
@@ -0,0 +1,13 @@
+package bg.uni.sofia.hiker.monti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MontiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/uni/sofia/hiker/monti/MontiApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.uni.sofia.hiker.monti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MontiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
