d1978b28885ac908f2ebb41b53e5752afbc8a629
Hristiyan Denkov <hristiyan.denkov@sap.com>
2025-03-16 13:36:38 +0200

    Enable CORS

--- src/main/java/bg/uni/sofia/hiker/monti/controller/MontiController.java
+++ src/main/java/bg/uni/sofia/hiker/monti/controller/MontiController.java
@@ -2,6 +2,7 @@ package bg.uni.sofia.hiker.monti.controller;
 
 import java.util.Set;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import bg.uni.sofia.hiker.monti.kafka.consumer.FeatureExtractor;
 
 @RestController
 @RequestMapping("/monti")
+@CrossOrigin(origins = "*")
 public class MontiController {
 
     private final FeatureExtractor featureExtractor = new FeatureExtractor();
